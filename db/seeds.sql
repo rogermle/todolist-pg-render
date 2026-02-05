edf1978b8a8a4d9f8e52e3a60808f4dbd0573d63
Roger Le <rogermle@gmail.com>
2026-02-04 19:47:18 -0600

    Moving folders and subfolders/files into root level

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, password) VALUES ('Alex', 'alex.museum@gmail.com', 'password12345');
+INSERT INTO users (name, email, password) VALUES ('Priya', 'priya.art@outlook.com', 'password12345');
+INSERT INTO users (name, email, password) VALUES ('Marco', 'marco.gallery@yahoo.com', 'password12345');
+INSERT INTO users (name, email, password) VALUES ('Sofia', 'sofia.curator@icloud.com', 'password12345');
+INSERT INTO users (name, email, password) VALUES ('Ethan', 'ethan.visitor@protonmail.com', 'password12345');
